4070284706a1a1338e0df08d740dc25775379fe1
Zulaika <zulaika.ali@outlook.com>
2022-10-26 16:33:34 +0100

    add form action

--- README.md
+++ README.md
@@ -6,10 +6,13 @@
 
 The project involved users entering a url which then returns a shortened URL:
 
--The backend will then generate a shortened path 
--No login should be required to create a shortened URL
--If User tries to access your website with a path you have stored in your database, they should get rerouted to the URL it relates to
--If User tries to access your website with a path you do not have stored in your database, they should get rerouted to the homepage where they can create a new short URL
+- The backend will then generate a shortened path 
+
+- No login should be required to create a shortened URL
+
+- If User tries to access your website with a path you have stored in your database, they should get rerouted to the URL it relates to
+
+- If User tries to access your website with a path you do not have stored in your database, they should get rerouted to the homepage where they can create a new short URL
 
 ## Installation & Use
 
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,16 +1,28 @@
-import React from "react"
+import React,{ useState } from "react"
+import axios from "axios"
 import './index.css'
 import logo from '../../static/logo.png'
 
 const Search = () => {
+    const [input,setInput] = useState()
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        setInput(e.target.value)
+        console.log(input)
+        axios.post("http://127.0.0.1:5000", { url: {input}})
+        .then ( response => console.log(response))
+        
+
+    };
+    
     return (
         <div className="search">
             <div id="img"><img src={logo} alt="logo" /></div>
-            <form action="heroku">
-            
+            <form action="" onSubmit={handleFormSubmit}>   
                 <label for="link"></label>
-                <input type="text" name="link-search" id="link" placeholder="enter link" />
-                <input type="submit" name="search" id = 'submit' value="Reduce URL" />
+                <input type="text" name="link-search" id="link" placeholder="enter link" value={input} />
+                <input type="submit"  name="search" id = 'submit' placeholder="Reduce URL" />
             </form>
         </div>
     )
