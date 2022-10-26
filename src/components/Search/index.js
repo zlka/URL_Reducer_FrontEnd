42980648065ac97ee7b87114fae8cfabd68cfb1e
Zulaika <zulaika.ali@outlook.com>
2022-10-26 18:03:28 +0100

    change handle post submit

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,35 +1,37 @@
-import React,{ useState } from "react"
+import React, { useState } from "react"
 import axios from "axios"
 import './index.css'
 import logo from '../../static/logo.png'
 
 const Search = () => {
-    const [input,setInput] = useState()
-    const [ data, setData ] = useState()
-    const [ hidden, setHidden] = useState(true)
+    const [input, setInput] = useState()
+    const [data, setData] = useState()
+    const [hidden, setHidden] = useState(true)
 
     const handleFormSubmit = (e) => {
         e.preventDefault()
-        console.log({input}.input)
-        const sendInput = {input}.input
-        axios.post("http://127.0.0.1:5000/random", { url: sendInput})
-        .then ( data => data.json())
-        .then ( data => setData(data))
-        console.log("data",data)
+        console.log({ input }.input)
+        const sendInput = { input }.input
+        axios.post("http://127.0.0.1:5000/random", { url: sendInput })
+        .then(response => {
+             data = response.data;
+             setData(data)
+            })
+        console.log("data", data)
         setHidden(!hidden)
     };
-    
+
     const handleInputChange = (e) => {
         setInput(e.target.value)
     };
-    
+
     return (
         <div className="search">
             <div id="img"><img src={logo} alt="logo" /></div>
-            <form action="" onSubmit={handleFormSubmit}>   
+            <form action="" onSubmit={handleFormSubmit}>
                 <label for="link"></label>
                 <input type="text" name="link-search" id="link" placeholder="enter link" value={input} onChange={handleInputChange} />
-                <input type="submit"  name="search" id = 'submit' placeholder="Reduce URL" />
+                <input type="submit" name="search" id='submit' placeholder="Reduce URL" />
             </form>
             <div hidden={hidden}>
                 <a href={data}>{data}</a>
